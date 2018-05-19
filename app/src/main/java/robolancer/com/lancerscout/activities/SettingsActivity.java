0ae6e32e83999f890714fde7911dd0a863196599
Brian Chen <brianchen0810@gmail.com>
2018-05-19 19:56:36 -0400

    Add Toolbar to Settings

--- app/src/main/java/robolancer/com/lancerscout/activities/SettingsActivity.java
+++ app/src/main/java/robolancer/com/lancerscout/activities/SettingsActivity.java
@@ -1,17 +1,42 @@
 package robolancer.com.lancerscout.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
 
+import robolancer.com.lancerscout.R;
 import robolancer.com.lancerscout.fragments.SettingsFragment;
+import robolancer.com.lancerscout.utilities.AppCompatPreferenceActivity;
 
-public class SettingsActivity extends AppCompatActivity{
+public class SettingsActivity extends AppCompatPreferenceActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
+        setupActionBar();
+
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+    }
+
+    private void setupActionBar() {
+        ViewGroup rootView = findViewById(R.id.action_bar_root);
+
+        if (rootView != null) {
+            View view = getLayoutInflater().inflate(R.layout.app_bar_layout, rootView, false);
+            rootView.addView(view, 0);
+
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+        }
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
