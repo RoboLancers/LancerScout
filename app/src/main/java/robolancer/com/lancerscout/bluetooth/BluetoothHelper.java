e262562f28efe9a3c5233418065f53ba9ec16beb
Brian Chen <brianchen0810@gmail.com>
2018-05-19 20:14:17 -0400

    Implement different UUIDs for different phone numbers

--- app/src/main/java/robolancer/com/lancerscout/bluetooth/BluetoothHelper.java
+++ app/src/main/java/robolancer/com/lancerscout/bluetooth/BluetoothHelper.java
@@ -4,7 +4,9 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.ParcelUuid;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -15,8 +17,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
+
+import robolancer.com.lancerscout.activities.SettingsActivity;
 
 public class BluetoothHelper implements Runnable{
 
@@ -61,7 +66,7 @@ public class BluetoothHelper implements Runnable{
             try {
                 dialog.dismiss();
                 BluetoothDevice device = deviceList.get(which);
-                ParcelUuid uuid = new ParcelUuid(UUID.fromString("fba199f7-47a7-4ed4-b880-3073424d2e2c" + ""));
+                ParcelUuid uuid = new ParcelUuid(UUID.fromString(getUUID() + ""));
                 BluetoothSocket socket = device.createRfcommSocketToServiceRecord(uuid.getUuid());
                 bluetoothAdapter.cancelDiscovery();
                 Toast.makeText(context, "Connecting to " + device.getName(), Toast.LENGTH_LONG).show();
@@ -74,6 +79,26 @@ public class BluetoothHelper implements Runnable{
                 showBluetoothDevices();
             }
         }).setCancelable(false).show();
+    }
+
+    public String getUUID(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        switch(sharedPreferences.getString("pref_scoutingPhoneNumber", "")){
+            case "Scouting Phone 1":
+                return "fba199f7-47a7-4ed4-b880-3073424d2e2c";
+            case "Scouting Phone 2":
+                return "2e02adb8-9ce7-45ee-9810-5ec6f3fff542";
+            case "Scouting Phone 3":
+                return "c2c7e59e-d5d5-4c70-a565-06f74f9d54cf";
+            case "Scouting Phone 4":
+                return "d33724ac-4c4c-4acf-a309-e3567acefbf8";
+            case "Scouting Phone 5":
+                return "126a612f-2ff3-4a81-a159-596c3a1ecfe8";
+            case "Scouting Phone 6":
+                return "0e2f69a9-16d1-4283-b00b-a246ac96fc93";
+            default:
+                return "";
+        }
     }
 
     @Override
